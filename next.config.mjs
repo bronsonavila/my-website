6e89f67fef4ae814d2c7b2deb11f52b7e6c0c6e9
Bronson Avila <bronson.avila@gmail.com>
2025-09-26 00:30:15 -1000

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: false,
   images: {
-    remotePatterns: [
-      {
-        hostname: 'images.ctfassets.net',
-        protocol: 'https'
-      }
-    ]
+    remotePatterns: [{ hostname: 'images.ctfassets.net', protocol: 'https' }]
   }
 }
 
